2e95d2c19047a17375b978f7324620aa4651ef64
Genesis Rodriguez <genesisnatasha2218@gmail.com>
2024-03-08 12:08:07 -0400

    se culminó el controlador de services, se colocó la ruta general como resources :services

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,57 @@
+class ServicesController < ApplicationController
+  def index
+    @services = Service.all
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def create
+    @service = Service.new(service_params)
+    @service.user_id = current_user.id
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    if current_user.id == @service.user.id
+      @service = set_service
+    else
+      redirect_to service_path(@service)
+    end
+  end
+
+  def update
+    @service = set_service
+    if @service.update(service_params)
+      redirect_to service_path(@service)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @service = Service.find(params[:id])
+    @service.destroy
+
+    redirect_to @service, status: :see_other
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:service_type, :title, :price, :rating, :description, :user_id, :address, :longitude, :latitude, :date, :photo)
+  end
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+end
