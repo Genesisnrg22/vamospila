fece6e39eddfca0ddc0916a30e860525bee3fe6d
Genesis Rodriguez <genesisnatasha2218@gmail.com>
2024-03-08 16:53:28 -0400

    avanzado el controlador del lineItems

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,23 +1,33 @@
 class LineItemsController < ApplicationController
   skip_before_action :authenticate_user!
   include CurrentCart
-  before_action :set_cart, only: :create
+  before_action :set_cart, only: [:create, :index]
 
   def index
+    cart = @cart
     @line_items = LineItem.all
+    if current_user.id
+      @line_items = LineItem.where(cart: cart.id)
+    #else
+      #@line_items = LineItem.where(cart: @cart)
+    end
   end
 
   def new
+    @service = Service.find(params[:service_id])
     @line_item = LineItem.new
   end
 
   def create
-    @cart = set_cart
+    @service = Service.find(params[:service_id])
+    @qtty = line_item_params[:qtty].to_i
+    @line_item = LineItem.new(service: @service, qtty: @qtty)
+    @line_item.price = @service.price * @qtty
     @line_item.cart = @cart
-    if @cart.save
-      redirect_to @cart, notice: 'Se ha añadido con éxito tu producto.'
+    if @line_item.save
+      redirect_to @line_items, notice: 'Se ha añadido con éxito tu producto.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -34,6 +44,10 @@ class LineItemsController < ApplicationController
   end
 
   private
+
+  def line_item_params
+    params.require(:line_item).permit(:qtty)
+  end
 
   def set_line_item
     @line_item = LineItem.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :services do
+    resources :line_items, only: [:create, :new]
+  end
+
+  resources :line_items, only: [:index, :destroy]
   # Defines the root path route ("/")
   # root "posts#index"
-  get resources :line_items
+
 end
