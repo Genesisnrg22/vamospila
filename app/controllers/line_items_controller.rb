b49767ad5877b0e6ce6485d5557877c12d863b8b
Genesis Rodriguez <genesisnatasha2218@gmail.com>
2024-03-08 15:36:33 -0400

    avanzando en el lineitems

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,19 +4,38 @@ class LineItemsController < ApplicationController
   before_action :set_cart, only: create
 
   def index
-    @lineitems = LineItem.all
+    @line_items = LineItem.all
   end
 
   def new
+    @line_item = LineItem.new
   end
 
   def create
-    @cart
+    @cart = set_cart
+    @line_item.cart = @cart
+    if @cart.save
+      redirect_to @cart, notice: 'Se ha añadido con éxito tu producto.'
+    else
+      render :new
+    end
   end
 
   def show
+    @line_item = set_line_item
+    @photo = @line_item.photo
   end
 
   def destroy
+    @line_item = LineItem.find(params[:id])
+    @line_item.destroy
+
+    redirect_to @line_item, status: :see_other
+  end
+
+  private
+
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
   end
 end
