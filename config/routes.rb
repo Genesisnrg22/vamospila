a1c0e1d9e2680c7dfd6929ce91bb8085b1a085e5
Jose Carrillo <jacg182010@gmail.com>
2024-03-11 15:22:58 -0600

    eliminar del carrito listo

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  delete "line_items/:item_id", to: "line_items#destroy", as: :line_item
+
   resources :services do
     resources :line_items, only: [:new, :create]
   end
@@ -14,7 +16,7 @@ Rails.application.routes.draw do
     resource :orders, only: [:update]
   end
 
-  resources :line_items, only: [:index, :destroy]
+  resources :line_items, only: [:index]
 
   resources :carts, only: [:show] do
     resources :orders, only: [:new]
@@ -23,5 +25,4 @@ Rails.application.routes.draw do
   # root "posts#index"
   post "line_items/minus", to: "line_items#minus"
   post "line_items/add", to: "line_items#add"
-  # post "line_items", to: "line_items#create"
 end
