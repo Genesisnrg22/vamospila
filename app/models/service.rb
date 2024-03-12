224f260582081c0df29da4fbe1ba76872457d9b5
javier galviz <jegalviz2111@gmail.com>
2024-03-12 14:42:41 -0400

    userprofile listo

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,6 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :line_items
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
