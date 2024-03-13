852bf0816f92fdbc9170b1f267aaa1efbe39682b
javier galviz <jegalviz2111@gmail.com>
2024-03-13 09:10:02 -0400

    mapbox zoom listo

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -33,6 +33,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 70, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 5, duration: 0 })
   }
 }
